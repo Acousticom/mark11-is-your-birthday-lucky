83fe546a0b796ab79b17306c15a90dab93bf78d8
Acousticom <omkarsrinivasa.2000@gmail.com>
2022-12-03 15:38:30 +0530

    doc

---
+++ index.js
@@ -0,0 +1,34 @@
+const dateOfBirth = document.querySelector("#date-of-birth")
+const luckyNumber = document.querySelector("#lucky-number")
+const checkNumberButton = document.querySelector("#check-number")
+const outputBox = document.querySelector("#output-box")
+
+function compareValues(sum, luckyNumber){
+    if(sum % luckyNumber===0){
+        outputBox.innerText=`${luckyNumber} is your lucky number!!!🥳 🥳  `
+    }
+    else{
+        outputBox.innerText=`${luckyNumber} is not your lucky number!!!😞😞`
+    }
+}
+
+function checkBirthDateIslucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if(dob && sum){
+    compareValues(sum, luckyNumber.value)}
+    else{
+    outputBox.innerText="Please enter both the values"
+    }   
+}
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","")
+    let sum = 0 ;
+    for( let i=0; i<dob.length; i++){
+        sum = sum + Number(dob.charAt(i))
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener("click",checkBirthDateIslucky)
